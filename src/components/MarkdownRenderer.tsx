897719015cfe73fe9a70c5d87775979aa97fac17
anuragparashar26 <anuragparashar5025@gmail.com>
2025-08-17 02:06:46 +0530

    added markdown features

--- src/components/MarkdownRenderer.tsx
+++ src/components/MarkdownRenderer.tsx
@@ -24,6 +24,12 @@ const MarkdownRenderer: React.FC<MarkdownRendererProps> = ({ content, className 
           h1: ({node, ...props}) => <h1 className="text-2xl font-bold mt-4 mb-2" {...props} />,
           h2: ({node, ...props}) => <h2 className="text-xl font-semibold mt-4 mb-2" {...props} />,
           h3: ({node, ...props}) => <h3 className="text-lg font-semibold mt-4 mb-2" {...props} />,
+          ul: ({node, ...props}) => <ul className="list-disc pl-6 my-2" {...props} />,
+          ol: ({node, ...props}) => <ol className="list-decimal pl-6 my-2" {...props} />,
+          li: ({node, ...props}) => <li className="mb-1" {...props} />,
+          blockquote: ({node, ...props}) => (
+            <blockquote className="border-l-4 border-gray-400 dark:border-gray-600 pl-4 italic text-gray-700 dark:text-gray-300 my-4" {...props} />
+          ),
           code(props: any) {
             const {inline, className: codeClassName, children, ...rest} = props;
             return !inline ? (
